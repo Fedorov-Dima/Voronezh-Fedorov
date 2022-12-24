2aa3ec2ea89c4525631759e4fd543dff73216f1d
Дима <fedorov.2006@yandex.ru>
2022-12-24 15:56:13 +0300

    Draw Ellipse

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+            self.do_paint = False
+
+    def draw_flag(self, qp):
+        d = random.randrange(1, 200)
+        x = random.randrange(100, 600)
+        y = random.randrange(100, 400)
+        qp.setBrush(QColor('yellow'))
+        qp.drawEllipse(x, y, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
